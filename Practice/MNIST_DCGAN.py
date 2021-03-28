cec9223d1ee4914a97445c5d7ae0b763abdbb197
Vincent McLoughlin <vincemcloughlin@hotmail.com>
2021-03-28 19:15:40 -0400

    Code written, can't test yet.

--- Practice/MNIST_DCGAN.py
+++ Practice/MNIST_DCGAN.py
@@ -23,9 +23,14 @@ train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
+noise_dim = 100
 
 # Batch and shuffle the data
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+cross_entropy = keras.losses.BinaryCrossentropy(from_logits=True)
+generator_optimizer = keras.optimizers.Adam(1e-4)
+discriminator_optimizer = keras.optimizers.Adam(1e-4)
 
 #The generator 
 def make_generator_model():
@@ -93,12 +98,58 @@ def train_step(images):
         disc_loss = discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator= = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
-def train():
+
+def generate_and_save_images(model, epoch, test_input):
+
+    predictions = model(test_input, training = False)
+
+    fig = plt.figure(figsize=(4,4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
+def train(dataset, epochs):
+    
+    num_examples_to_generate = 16
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
+    
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                discriminator_optimizer=discriminator_optimizer,
+                                generator=generator
+                                discriminator=discriminator)
+    for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch)
+
+        #Produce images for the gif as you go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epoch+1, seed)
+
+        #Save every 15 epochs 
+        if (epoch+1)%15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,
+                               epochs,
+                               seed)
 
 generator = make_generator_model()
 noise = tf.random.normal([1, 100])
@@ -109,20 +160,6 @@ discriminator = make_discriminator_model()
 #decision = discriminator(generated_image)
 #print (decision)
 
-cross_entropy = keras.losses.BinaryCrossentropy(from_logits=True)
-generator_optimizer = keras.optimizers.Adam(1e-4)
-discriminator_optimizer = keras.optimizers.Adam(1e-4)
-
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                discriminator_optimizer=discriminator_optimizer,
-                                generator=generator
-                                discriminator=discriminator)
-
-#Define Training Loop 
 EPOCHS = 80 
-noise_dim = 100 
-num_examples_to_generate = 16
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
+train(train_dataset, EPOCHS)
